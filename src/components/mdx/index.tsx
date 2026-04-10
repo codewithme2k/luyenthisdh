e018d4aac97c018312543f20cd738358f21e5f55
Code With Me <codewithme2k@gmail.com>
2026-04-10 22:02:41 +0700

    fix: resolve ReferenceError by dynamically importing Zoom component

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -1,5 +1,6 @@
+
 import type { MDXComponents } from "mdx/types";
-import { Image, Zoom, type ImageProps } from "@/components/customs/image";
+import { Image, type ImageProps } from "@/components/customs/image";
 
 import { Twemoji } from "@/components/customs/twemoji";
 import { CodeTitle } from "./code-title";
@@ -7,6 +8,14 @@ import { Pre } from "./pre";
 import { TableWrapper } from "./table-wrapper";
 import Link from "next/link";
 import SOFA2Calculator from "@/components/tools/SOFA2Calculator";
+import dynamic from "next/dynamic";
+
+const Zoom = dynamic(
+  () => import("@/components/customs/image").then((mod) => mod.Zoom),
+  {
+    ssr: false,
+  },
+);
 
 export const MDX_COMPONENTS: MDXComponents = {
   Image: ({ alt, ...rest }: ImageProps) => {
